68b3244826a4de819026562f1360944c35a03446
Snehal1010 <166135875+patelsnehal9819@users.noreply.github.com>
2026-03-08 22:08:20 +0000

    Enable PWA

--- sportsxpress-frontend/src/index.js
+++ sportsxpress-frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 console.log('🔴 Index.js is running');
 console.log('🔴 App component:', App);
@@ -12,5 +13,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// Register service worker for PWA
+serviceWorkerRegistration.register();
 
 console.log('🔴 Render called');
